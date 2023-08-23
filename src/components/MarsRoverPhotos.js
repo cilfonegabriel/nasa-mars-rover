f5d91dcef2877c4e78c33e05ebb1a6dc6637fbfa
gabicilfone <cilfonegabriel@gmail.com>
2023-08-23 17:31:58 -0300

    add linters husky, connect with API and show photo

--- src/components/MarsRoverPhotos.js
+++ src/components/MarsRoverPhotos.js
@@ -2,25 +2,36 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const MarsRoverPhotosPage = () => {
+  const [rover, setRover] = useState('');
   const [photos, setPhotos] = useState([]);
   const apiKey = 'mn0cL646A86fzVD3vI3MdMpphxncHeUDjNCzgPja';
 
   useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`
-      )
-      .then((response) => {
-        setPhotos(response.data.photos);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+    if (rover) {
+      axios
+        .get(
+          `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=1000&api_key=${apiKey}`,
+        )
+        .then((response) => {
+          setPhotos(response.data.photos);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }
+  }, [rover]);
+
+  const handleRoverChange = (selectedRover) => {
+    setRover(selectedRover);
+  };
 
   return (
     <div>
-      <h1>Mars Rover Photos</h1>
+      <div>
+        <button type="button" onClick={() => handleRoverChange('curiosity')}>Curiosity</button>
+        <button type="button" onClick={() => handleRoverChange('opportunity')}>Opportunity</button>
+        <button type="button" onClick={() => handleRoverChange('spirit')}>Spirit</button>
+      </div>
       {photos.length > 0 ? (
         <div>
           {photos.map((photo) => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
-import React from "react";
-import MarsRoverPhotosPage from "@/components/MarsRoverPhotos";
+import React from 'react';
+import Link from 'next/link';
 
-const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Rover Photos App</h1>
-            <MarsRoverPhotosPage />
-        </div>
-    )
-}
+const MainPage = () => (
+  <div className="main-page">
+    <h1>Welcome to the Mars Rover Photos App</h1>
+    <p>Explore the wonders of Mars with NASA&apos;s rover photos!</p>
+    <Link href="/roverphotos">
+      <div className="rover-button">Explore Rover Photos</div>
+    </Link>
+  </div>
+);
 
-export default HomePage;
+export default MainPage;
